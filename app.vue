d694ff80354577b6202197e5d6f80fe43915f594
Ricky <rickyraveanu02@gmail.com>
2024-04-14 17:16:03 +0200

    seo & future fonts

--- app.vue
+++ app.vue
@@ -1,10 +1,12 @@
 <template>
-  <Toaster />
-  <div class="container relative flex h-svh flex-col items-center justify-center md:justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+  <Toaster/>
+  <div
+      class="container relative flex h-svh flex-col items-center justify-center md:justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
     <div class="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
-      <div class="absolute inset-0 bg-zinc-900" />
+      <div class="absolute inset-0 bg-zinc-900"/>
       <div class="gap-2 relative z-20 flex items-center text-lg font-medium">
-        <School/> EduPulse
+        <School/>
+        EduPulse
       </div>
       <div class="relative z-20 mt-auto">
         <blockquote class="space-y-2">
@@ -27,7 +29,7 @@
             Enter your email below to sign up for the launch
           </p>
         </div>
-        <UserAuthForm />
+        <UserAuthForm/>
         <p class="px-8 text-center text-sm text-muted-foreground">
           By clicking Sign Up, you agree to selling your soul.
         </p>
@@ -38,8 +40,38 @@
 
 <script lang="ts" setup>
 import UserAuthForm from './components/UserAuthForm.vue';
-import { cn } from '@/lib/utils';
-import { buttonVariants } from '@/components/ui/button';
 import Toaster from '@/components/ui/toast/Toaster.vue'
-import { School } from 'lucide-vue-next';
+import {School} from 'lucide-vue-next';
+
+useSeoMeta({
+  title: 'EduPulse',
+  description: 'EduPulse is a revolutionary platform designed to manage and balance academic workload between teachers and pupils. It helps in monitoring student workload, offering insights into their efforts and study time, thereby promoting a healthier educational environment.',
+  ogUrl() {
+    return 'www.edupulse.rickyraveanu.com';
+  },
+  ogTitle() {
+    return 'EduPulse';
+  },
+  ogDescription() {
+    return 'EduPulse is a revolutionary platform designed to manage and balance academic workload between teachers and pupils. It helps in monitoring student workload, offering insights into their efforts and study time, thereby promoting a healthier educational environment.';
+  },
+  ogSiteName() {
+    return 'EduPulse';
+  },
+  ogImage() {
+    return 'https://www.edupulse.rickyraveanu.com/edupulse_og.png';
+  },
+  ogLocale() {
+    return 'en_US';
+  },
+  ogType() {
+    return 'website';
+  },
+  twitterCard() {
+    return 'summary_large_image';
+  },
+  twitterTitle() {
+    return 'EduPulse';
+  },
+})
 </script>
