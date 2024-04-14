7a4efc247e1175ea7005d2d31b0c3a724f4d03e6
Ricky <rickyraveanu02@gmail.com>
2024-04-14 18:23:35 +0200

    refactor seo

--- app.vue
+++ app.vue
@@ -46,32 +46,22 @@ import {School} from 'lucide-vue-next';
 useSeoMeta({
   title: 'EduPulse',
   description: 'EduPulse is a revolutionary platform designed to manage and balance academic workload between teachers and pupils. It helps in monitoring student workload, offering insights into their efforts and study time, thereby promoting a healthier educational environment.',
-  ogUrl() {
-    return 'www.edupulse.rickyraveanu.com';
-  },
-  ogTitle() {
-    return 'EduPulse';
-  },
-  ogDescription() {
-    return 'EduPulse is a revolutionary platform designed to manage and balance academic workload between teachers and pupils. It helps in monitoring student workload, offering insights into their efforts and study time, thereby promoting a healthier educational environment.';
-  },
-  ogSiteName() {
-    return 'EduPulse';
-  },
-  ogImage() {
-    return 'https://edupulse.rickyraveanu.com/edupulse_og.png';
-  },
-  ogLocale() {
-    return 'en_US';
-  },
-  ogType() {
-    return 'website';
-  },
-  twitterCard() {
-    return 'summary_large_image';
-  },
-  twitterTitle() {
-    return 'EduPulse';
+
+  ogUrl: 'www.edupulse.rickyraveanu.com',
+  ogTitle: 'EduPulse',
+  ogDescription: 'EduPulse is a revolutionary platform designed to manage and balance academic workload between teachers and pupils. It helps in monitoring student workload, offering insights into their efforts and study time, thereby promoting a healthier educational environment.',
+  ogSiteName: 'EduPulse',
+  ogImage: 'https://edupulse.rickyraveanu.com/edupulse_og.png',
+  ogLocale: 'en_US',
+  ogType: 'website',
+
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'EduPulse',
+
+  robots: {
+    index: true,
+    follow: true,
+    noarchive: true,
   },
 })
 </script>
