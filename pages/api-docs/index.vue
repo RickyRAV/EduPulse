832af8051faef3c3e344b5154360e1f1e51c8af0
Ricky <rickyraveanu02@gmail.com>
2024-04-22 01:40:56 +0200

    add OpenAPI 3.0 definitions

---
+++ pages/api-docs/index.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import SwaggerUI from 'swagger-ui-dist/swagger-ui-bundle.js';
+import 'swagger-ui-dist/swagger-ui.css';
+
+onMounted(() => {
+  SwaggerUI({
+    dom_id: '#swagger-ui',
+    url: '/openapi.yaml'
+  });
+});
+</script>
+
+<template>
+  <div>
+    <div id="swagger-ui"></div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
