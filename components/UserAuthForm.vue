5f55c8800fb99b35e29c6e63cc0db872612eb008
Ricky <rickyraveanu02@gmail.com>
2024-04-26 12:37:14 +0200

    Update UserAuthForm.vue

--- components/UserAuthForm.vue
+++ components/UserAuthForm.vue
@@ -74,20 +74,6 @@ const onSubmit = handleSubmit(async (values) => {
         </div>
       </FormField>
     </form>
-<!--    <div classes="relative">-->
-<!--      <div classes="absolute inset-0 flex items-center">-->
-<!--        <span classes="w-full border-t" />-->
-<!--      </div>-->
-<!--      <div classes="relative flex justify-center text-xs uppercase">-->
-<!--        <span classes="bg-background px-2 text-muted-foreground">-->
-<!--          Or continue with-->
-<!--        </span>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <Button variant="outline" type="button">-->
-<!--      <GithubLogoIcon classes="mr-2 h-4 w-4" />-->
-<!--      GitHub-->
-<!--    </Button>-->
   </div>
 </template>
 
