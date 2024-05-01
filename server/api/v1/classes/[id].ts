595f8d784207002b3e1250356bb9f5e43a80a6a6
Ricky <rickyraveanu02@gmail.com>
2024-05-01 13:24:05 +0200

    trying to remove middleware

--- server/api/v1/classes/[id].ts
+++ server/api/v1/classes/[id].ts
@@ -24,22 +24,22 @@ export default defineEventHandler(async (event) => {
     try {
         const {id} = router.data;
         const {offset, limit} = query.data;
-        const {user: {id: user_id}} = event.context.user;
+        // const {user: {id: user_id}} = event.context.user;
         const data = await db.select()
             .from(courses)
             .where(eq(courses.classId, id))
             .orderBy(desc(courses.createdAt))
             .limit(limit)
             .offset(offset);
-        const total_records = await db.select({ count: count() })
-            .from(courses)
-            .innerJoin(coursesTeachers, eq(courses.id, coursesTeachers.coursesId))
-            .innerJoin(teachers, eq(coursesTeachers.teachersId, teachers.userId))
-            .where(eq(courses.classId, id));
+        // const total_records = await db.select({ count: count() })
+        //     .from(courses)
+        //     .innerJoin(coursesTeachers, eq(courses.id, coursesTeachers.coursesId))
+        //     .innerJoin(teachers, eq(coursesTeachers.teachersId, teachers.userId))
+        //     .where(eq(courses.classId, id));
         setResponseStatus(event, 200);
         return {
             data,
-            pagination: { total_records: total_records[0].count }
+            // pagination: { total_records: total_records[0].count }
         }
     } catch (error) {
         setResponseStatus(event, 500);
